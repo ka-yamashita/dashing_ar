14ee0105d69c7c6db09826edd0a8450203d66c3b
ka-yamashita <yamashitakazuhiko@kazuhiko-no-MacBook-Air.local>
2015-03-23 01:15:31 +0900

    接続先を起動変数から取得するようにした

--- lib/initialize.rb
+++ lib/initialize.rb
@@ -1,4 +1,4 @@
 require 'active_record'
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
-ActiveRecord::Base.establish_connection(:development)
+ActiveRecord::Base.establish_connection(ENV['RACK_ENV'].to_sym)
